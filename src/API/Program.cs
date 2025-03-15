6b5910261e53dcd39b726dc98d32916d58c002ff
Gaurav Saini <gsaini313@gmail.com>
2025-03-15 09:33:48 -0400

    latest update

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -3,8 +3,9 @@ using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
