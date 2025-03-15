2161b0168de3da0ef12c643aacc8e67a7f5095b8
Gaurav Saini <gsaini313@gmail.com>
2025-03-15 09:40:03 -0400

    swagger load fix

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -23,7 +23,11 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Utilities API");
+    c.RoutePrefix = string.Empty;
+});
 
 app.UseHttpsRedirection();
 
